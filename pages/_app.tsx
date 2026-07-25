ea02b3ac3cd93701ee71073121d79eb8c6028880
armjukl <armjukl@163.com>
2026-07-25 14:52:49 +0800

    Add browser title and favicon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,20 @@
 // pages/_app.tsx
 import '../styles/globals.css';
+import Head from 'next/head';
 import { AppProps } from 'next/app';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>Arc-music</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+        <meta name="theme-color" content="#6366f1" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
